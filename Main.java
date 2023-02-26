26da6a475a69d1e36a6924e922744ea3c064de00
Kacper.2 <114801446+kacper2TI@users.noreply.github.com>
2023-02-26 12:06:18 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+package aplikacja;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("program.fxml"));
+        primaryStage.setTitle("LOGIN");
+        primaryStage.setScene((new Scene(root, 600, 400)));
+        primaryStage.show();
+    }
+}
