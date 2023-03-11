7b823959840a5f0536435d5322228bb904cb5ec6
Kacper2TI <114801446+kacper2TI@users.noreply.github.com>
2023-03-11 10:06:30 +0100

    Update Godz.py

--- Godz.py
+++ Godz.py
@@ -1,7 +1,7 @@
-h = int(input("Czas startu (godziny): "))
+h = int(input("Czas startu (godziny): ")) #wprowadzamy wszystkie dane 
 m = int(input("Czas startu (minuty) "))
 d = int(input("Czas trwania wydarzenia (minuty): "))
-print("Wydarzenia zaczyna sie o %d:%d i trwa %d minut" %(h,m,d))
+print("Wydarzenia zaczyna sie o %d:%d i trwa %d minut" %(h,m,d)) 
 godz = ((m + d)//60 + h) % 24
 minuta = (m + d) % 60
 #wariant1
